5d5fa348945589d56ee7f45cb12db2fe7454078b
Henri Toukkari <90309793+henritoukkari@users.noreply.github.com>
2024-04-05 16:42:17 +0100

    Update game.js
    I have updated Simon's game. A user can select between green, red, blue, or yellow. This data will be stored in the variable so that I can keep track of the user's selections. I have also added a class that will flash when the user presses a selected button. There's also a corresponding sound effect to each button.

--- game.js
+++ game.js
@@ -1,17 +1,67 @@
+
+let level = 0;
+const started = false;
+
 const buttonColours = ["red", "blue", "green", "yellow"];
 
 const gamePattern = [];
+const userClickedPattern = [];
+
+
+$(document).keypress(function() {
+  if (!started) {
+
+    
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  const userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  console.log(userChosenColour);
+  console.log(userClickedPattern);
+  
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  
+  level++
+  $("#level-title").text("Level " + level);
+
+});
 
 function nextSequence() {
 
-const randomNumber = Math.floor(Math.random() * 4);
-
-const randomChosenColour = buttonColours[randomNumber];
-  console.log(randomChosenColour);
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+  
 
-$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
+  
+  playSound(randomChosenColour);
+  currentColour(randomChosenColour);
 }
 
-nextSequence();
+
+function playSound(name) {
+
+  
+  const audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function() {
+      $("#" + currentColour).removeClass("pressed");
+  }, 200);
+  
+}
